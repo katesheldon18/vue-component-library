71fb0015e83c868be19f6f47e19b146c2a096f43
Kate <katshe18@gmail.com>
2025-07-03 21:19:37 +1200

    feat(app): basic routing and layout

---
+++ src/app/pages/DocsHome.vue
@@ -0,0 +1,21 @@
+
+
+<template>
+  <section>
+    <h1>Vue Component Library</h1>
+  </section>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+section {
+  padding: 2rem;
+}
+
+h1 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+</style>
