f07596f07a6446bca7d35a2212bb6d588523beee
Kate <katshe18@gmail.com>
2025-09-13 08:54:13 +1200

    feat(accordion): add accordion component with open/close logic

---
+++ src/app/pages/Accordions.vue
@@ -0,0 +1,17 @@
+<template>
+  <Accordion @click="onClick('test1')" :open="openId === 'test1'" />
+  <Accordion @click="onClick('test2')" :open="openId === 'test2'"/>
+  <Accordion @click="onClick('test3')" :open="openId === 'test3'"/>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import Accordion from "../../components/Accordion.vue";
+
+let openId = ref< string | null >('test1')
+
+function onClick(id: string) {
+  openId.value = openId.value === id ? null : id;
+}
+</script>
---
+++ src/components/Accordion.vue
@@ -0,0 +1,49 @@
+<template>
+  <div @click="onClick" class="accordion ">
+    <div class="accordion__icon">+</div>
+    <div class="accordion__title">Question</div>
+    <div :class="['accordion__wrapper', { 'is-open' : open }]" >
+      <div class="accordion__content">Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer Answer</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { onClick, open = false, } = defineProps<{ onClick?: (e: MouseEvent) => void; open: boolean; }>()
+</script>
+
+
+<style scoped>
+
+.accordion {
+  display: grid;
+  grid-template-columns: 2rem 1fr;
+  grid-template-rows: auto 1fr;
+  place-items: center start;
+}
+
+.accordion__icon {
+  grid-area: 1 / 1;
+}
+
+.accordion__title {
+  grid-area: 1 / 2;
+
+}
+
+.accordion__wrapper {
+  display: grid;
+  grid-area: 2 / 2;
+  grid-template-rows: 0fr;
+  transition: grid-template-rows 0.5s ease-in-out;
+}
+
+.accordion__wrapper.is-open {
+  grid-template-rows: 1fr;
+}
+
+.accordion__content {
+  overflow: hidden;
+}
+
+</style>
