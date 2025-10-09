d1e705e63f067610eaf310bd3d55b7c760c33af8
Kate <katshe18@gmail.com>
2025-10-10 10:33:24 +1300

    feat(checkbox): add custom svg slot, styling, additional checkbox examples

--- src/app/pages/Checkboxes.vue
+++ src/app/pages/Checkboxes.vue
@@ -1,13 +1,43 @@
 <template>
   <H1>Checkbox</H1>
-  <Checkbox v-model="isChecked" />
-  <div>{{isChecked}}</div>
+  <div class="spacing">
+    <Checkbox v-model="isChecked1" label="Checkbox - default" />
+    <div class="spacing">Checked: {{isChecked1}}</div>
+      <Checkbox v-model="isChecked2" label="Checkbox with custom checkmark (svg slot)" class="checkbox-example">
+        <template #checkmark>
+          <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#276014" stroke-width="20" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M30 120 C 90 250, 80 60, 210 0" />
+          </svg>
+        </template>
+      </Checkbox>
+    <div class="spacing">Checked: {{isChecked2}}</div>
+      <Checkbox v-model="isChecked3" label="Checkbox - checked by default"/>
+    <div class="spacing">Checked: {{isChecked3}}</div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import {ref} from "vue";
 import Checkbox from "../../components/Checkbox.vue";
-
-const isChecked = ref(false);
-
+const isChecked1 = ref(false);
+const isChecked2 = ref(false);
+const isChecked3 = ref(true);
 </script>
+
+<style lang="scss">
+@import "../styles/variables.scss";
+
+.spacing {
+  margin: 1rem 0;
+}
+
+.checkbox-example {
+  .checkbox__input {
+    border: 1px solid $color-success-border;
+  }
+
+  .checkbox__input:checked {
+    background: none;
+  }
+}
+</style>
--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,14 +1,23 @@
 <template>
-  <label>
-    <input type="checkbox" :checked="modelValue" @change="onChange" />
-    <span>label</span>
+  <label class="checkbox" :class="className">
+    <input type="checkbox" :checked="modelValue" @change="onChange" class="checkbox__input" />
+    <span class="checkbox__checkmark" :class="{'is-checked': modelValue}">
+      <slot name="checkmark">
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 15l5 5L19 7" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
+      </slot>
+    </span>
+    <span class="checkbox__label">{{ label }}</span>
   </label>
 </template>
 
 
 <script setup lang="ts">
 const props = defineProps<{
-  modelValue: boolean
+  modelValue: boolean,
+  label: string,
+  className?: string,
 }>();
 const emit = defineEmits(['update:modelValue']);
 
@@ -16,3 +25,54 @@ const onChange = (e) => {
   return emit('update:modelValue', e.target.checked);
 }
 </script>
+
+<style scoped lang="scss">
+@import "../app/styles/variables.scss";
+
+.checkbox {
+  cursor: pointer;
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  position: relative;
+}
+
+.checkbox__input {
+  appearance: none;
+  -webkit-appearance: none;
+  width: 1rem;
+  height: 1rem;
+  border: 1px solid $color-primary;
+  border-radius: 0.25rem;
+  background-color: white;
+  position: relative;
+
+  &:checked {
+    background-color: $color-primary;
+  }
+}
+
+.checkbox__checkmark {
+  display: none;
+  position: absolute;
+  width: 1rem;
+  height: 1rem;
+  pointer-events: none;
+  color: $white;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  align-items: center;
+  justify-content: center;
+}
+
+.checkbox__checkmark.is-checked {
+  display: inline-flex;
+}
+
+.checkbox__label {
+  color: $color-text;
+  font-size: $text-body;
+}
+
+</style>
