9efd7f34c0598934ac0829da97863f02e2195920
Kate <katshe18@gmail.com>
2026-06-09 11:58:16 +1200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [vue()],
-    test: {
-        environment: 'jsdom',
-    },
-})
+  plugins: [vue()],
+});
